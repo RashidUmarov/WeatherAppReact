cae5dcbcd02875adc85c7312fd1ee795521954a6
Rashid Umarov <rasrasras2014@mail.ru>
2023-06-07 21:09:11 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends React.Component{
+    render(){
+        return (<div>
+            <h1> Hello Weather APP</h1>
+        </div>
+        );
+    }
+}
+
+export default App;
